8c1a8d5165f1f2f8c06b1ba3de2008577055c102
Alan Oliveira <7979908+alanjholiveira@users.noreply.github.com>
2022-07-10 15:18:52 -0300

    Update UtilsTest.java

--- api/src/test/java/tech/dock/desafio/api/domain/utils/UtilsTest.java
+++ api/src/test/java/tech/dock/desafio/api/domain/utils/UtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.dock.desafio.api.infrastructure.config.testcontainers.AbstractIntegrationTest;
 
 import java.math.BigDecimal;
 
@@ -11,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-public class UtilsTest {
+public class UtilsTest extends AbstractIntegrationTest {
 
     @Test
     void quandoVerificarValorRetornaSucesso() {
